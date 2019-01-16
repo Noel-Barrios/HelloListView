8b9b0ca6911b0c6bd1f6c5855318f9affb8868b8
Noel Barrios <noel.barrios@optum.com>
2019-01-16 17:47:18 -0600

    create a simple ListView

---
+++ HelloListView/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HelloListView
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+
+            ListViewIs.ItemsSource = new List<string>
+            {
+                "Hello", "World", "ListViewDemonstration"
+            };
+        }
+
+
+    }
+}
